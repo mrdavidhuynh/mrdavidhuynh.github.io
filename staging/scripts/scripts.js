3d6242e9a957698b625e9beb9de546ada7d9f02b
David <hyhdvd@gmail.com>
2016-10-15 13:23:01 -0700

    update scroll reveals

--- staging/scripts/scripts.js
+++ staging/scripts/scripts.js
@@ -1,14 +1,24 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+sr.reveal('.js-fade-load',
+{
+  distance: '7.5rem',
+  scale: 1,
+  viewFactor: 0.1,
+  opacity: 0
+}, 100);
+
+sr.reveal('.js-fade-content',
+{
+  distance: '7.5rem',
+  scale: 1,
+  viewFactor: 0.33,
+  opacity: 0
+});
+
+sr.reveal('.js-fade-work',
+{
+  distance: '6rem',
+  scale: 1,
+  viewFactor: 1,
+  duration: 700,
+  opacity: 0
 });

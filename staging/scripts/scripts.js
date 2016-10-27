f211f03db0ad80c56d7d62fdd499862699405eeb
David Huynh <david@axiomzen.co>
2016-10-26 17:24:14 -0700

    update images and animations

--- staging/scripts/scripts.js
+++ staging/scripts/scripts.js
@@ -2,9 +2,19 @@ sr.reveal('.js-fade-load',
 {
   distance: '7.5rem',
   scale: 1,
-  viewFactor: 0.1,
+  viewFactor: .1,
   duration: 600,
   opacity: 0
+}, 100);
+
+sr.reveal('.js-fade-load2',
+{
+  distance: '6rem',
+  scale: 1,
+  viewFactor: .1,
+  duration: 600,
+  opacity: 0,
+  delay: 150
 }, 100);
 
 sr.reveal('.js-fade-content',
@@ -12,14 +22,5 @@ sr.reveal('.js-fade-content',
   distance: '7.5rem',
   scale: 1,
   viewFactor: 0.33,
-  opacity: 0
-});
-
-sr.reveal('.js-fade-work',
-{
-  distance: '6rem',
-  scale: 1,
-  viewFactor: 1,
-  duration: 700,
   opacity: 0
 });
